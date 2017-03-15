2742b680389a92690c9d7503f1aed65d1fda3cd1
sneak44 <benny.schott@gmail.com>
2017-03-15 18:02:21 +0100

    update

--- examples/tests/Springer/springer_happy.js
+++ examples/tests/Springer/springer_happy.js
@@ -6,7 +6,7 @@ module.exports = {
       browser.resizeWindow(1920, 1080);
   },
 
-  'search for Education' :function(client) {
+  'search for Education and validating' :function(client) {
     client
       .setValue('.//*[@id="query"]',['Education',client.Keys.ENTER])
       client.waitForElementPresent(".//*[@id='kb-nav--main']/div[1]/h1",1000)
